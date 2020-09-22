8cbcc82ace65b5f3477e6316585c9ca5d13691a4
j10181996 <52480601+j10181996@users.noreply.github.com>
2020-09-22 12:12:56 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # pixiv-backup
-backup the illusts and novels you bookmarked in pixiv
+backup the illusts and novels you have bookmarked in pixiv
---
+++ index.js
@@ -0,0 +1,225 @@
+const fs = require('fs');
+const moment = require('moment');
+const config = require('config');
+const PixivAppApi = require("pixiv-app-api");
+const PixivApi = require('pixiv-api-client');
+const pixivImg = require('pixiv-img');
+
+const pixiv = new PixivApi();
+const appPixiv = new PixivAppApi();
+let id;
+const tags = config.get('tags');
+
+const checkPath = (path) => {
+    let newPath = path;
+    newPath = newPath.replace(/\</g, '＜');
+    newPath = newPath.replace(/\>/g, '＞');
+    newPath = newPath.replace(/\:/g, '：');
+    newPath = newPath.replace(/\"/g, '＂');
+    newPath = newPath.replace(/\|/g, '｜');
+    newPath = newPath.replace(/\?/g, '？');
+    newPath = newPath.replace(/\*/g, '＊');
+    return newPath;
+}
+
+const novelBackup = async (tag, doSearch) => {
+    let novels;
+    try {
+        if (doSearch) {
+            novels = (await appPixiv.searchNovel(tag)).novels;
+        }
+        else {
+            novels = (await appPixiv.next()).novels;
+        }
+
+        for (let i = 0; i < novels.length; ++i) {
+            const novel = novels[i];
+            if (novel.isBookmarked) {
+                let path = `./novels/${tag}/${novel.id}-${novel.title.replace(/\//g, '／')}.txt`;
+                path = checkPath(path);
+                if (!fs.existsSync(path)) {
+                    pixiv.novelText(novel.id).then(res => {
+                        try {
+                            fs.writeFileSync(path, res.novel_text.replace(/\n/g, '\n　　'));
+                            console.log(path);
+                        }
+                        catch (e) {
+                            console.log(e);
+                        }
+                    }).catch(e => console.log(e));
+                }
+                else {
+                    continue;
+                }
+            }
+        }
+        if (novels && novels.length !== 0) {
+            const create_date_last = moment(novels[novels.length - 1].createDate).format('YYYY-MM-DD');
+            return create_date_last;
+        }
+    }
+    catch (e) {
+        if (e.response) {
+            if (e.response.status === 403) {
+                return 'done';
+            }
+        }
+        console.log(e);
+        fs.appendFile('./log.txt', `${e}\n`, (e) => console.log(e));
+    }
+}
+
+const illustBackup = async (doSearch) => {
+    let illusts, upToDate = false;
+    try {
+        if (doSearch) {
+            illusts = (await appPixiv.userBookmarksIllust(id)).illusts;
+        }
+        else {
+            illusts = (await appPixiv.next()).illusts;
+        }
+
+        for (let i = 0; i < illusts.length; ++i) {
+            const illust = illusts[i];
+            const tag = illust.tags.find(item => tags.indexOf(item.name) > -1);
+            if (illust.title) {
+                if (illust.metaPages.length !== 0) {
+                    let dir = `./illusts${tag ? `/${tag.name}` : ''}/${illust.id}-${illust.title.replace(/\//g, '／')}`;
+                    dir = checkPath(dir);
+                    if (!fs.existsSync(dir)) {
+                        fs.mkdirSync(dir);
+                        console.log(dir);
+                    }
+                    else {
+                        upToDate = true;
+                        continue;
+                    }
+                    for (let j = 0; j < illust.metaPages.length; ++j) {
+                        const img = illust.metaPages[j];
+                        const url = img.imageUrls.original;
+                        const arr = url.split('\.');
+                        const path = `${dir}/p${j}.${arr[arr.length - 1]}`;
+                        try {
+                            pixivImg(url, path);
+                        }
+                        catch (e) {
+                            console.log(e);
+                        }
+                    }
+                }
+                else {
+                    const url = illust.metaSinglePage.originalImageUrl;
+                    const arr = url.split('\.');
+                    let path = `./illusts${tag ? `/${tag.name}` : ''}/${illust.id}-${illust.title.replace(/\//g, '／')}.${arr[arr.length - 1]}`;
+                    path = checkPath(path);
+                    if (!fs.existsSync(path)) {
+                        try {
+                            pixivImg(url, path);
+                            console.log(path);
+                        }
+                        catch (e) {
+                            console.log(e);
+                        }
+                    }
+                    else {
+                        upToDate = true;
+                        continue;
+                    }
+                }
+            }
+        }
+        if (upToDate) {
+            return 'done';
+        }
+    }
+    catch (e) {
+        if (e.response) {
+            if (e.response.status === 403) {
+                return 'done';
+            }
+        }
+        console.log(e);
+        fs.appendFile('./log.txt', `${e}\n`, (e) => console.log(e));
+    }
+}
+
+const app = async () => {
+    const username = config.get('account');
+    const password = config.get('password');
+    const lastBackup = config.get('lastBackup');
+    const novelCycle = config.get('novelCycle');
+    const illustCycle = config.get('illustCycle');
+    const doNovelBackup = config.get('doNovelBackup');
+    try {
+        id = (await appPixiv.login(username, password)).user.id;
+        await pixiv.login(username, password);
+    }
+    catch (e) {
+        console.log("login failed");
+    }
+
+    if (!fs.existsSync('./novels')) {
+        fs.mkdirSync('./novels');
+    }
+    if (!fs.existsSync('./illusts')) {
+        fs.mkdirSync('./illusts');
+    }
+    tags.forEach(tag => {
+        if (!fs.existsSync(`./novels/${tag}`)) {
+            fs.mkdirSync(`./novels/${tag}`);
+        }
+    });
+    tags.forEach(tag => {
+        if (!fs.existsSync(`./illusts/${tag}`)) {
+            fs.mkdirSync(`./illusts/${tag}`);
+        }
+    });
+
+    let novelDate;
+    let tagIndex = 0;
+    let doSearch = true;
+    if (doNovelBackup) {
+        const novelInterval = setInterval(async () => {
+            novelDate = await novelBackup(tags[tagIndex], doSearch);
+            doSearch = false;
+            if (novelDate === 'done' || moment(novelDate).isBefore(lastBackup)) {
+                ++tagIndex;
+                doSearch = true;
+                if (tagIndex >= tags.length) {
+                    console.log('Done Novel Backup!');
+                    clearInterval(novelInterval);
+                    try {
+                        const json = fs.readFileSync('./config/default.json');
+                        const obj = JSON.parse(json);
+                        obj.lastBackup = moment().format('YYYY-MM-DD');
+                        fs.writeFileSync('./config/default.json', JSON.stringify(obj));
+                    }
+                    catch (e) {
+                        console.log(e);
+                    }
+
+                    const illustInterval = setInterval(async () => {
+                        const res = await illustBackup(doSearch);
+                        doSearch = false;
+                        if (res === 'done') {
+                            console.log('Done Illust Backup!');
+                            clearInterval(illustInterval);
+                        }
+                    }, illustCycle);
+                }
+            }
+        }, novelCycle);
+    }
+    else {
+        const illustInterval = setInterval(async () => {
+            const res = await illustBackup(doSearch);
+            doSearch = false;
+            if (res === 'done') {
+                console.log('Done Illust Backup!');
+                clearInterval(illustInterval);
+            }
+        }, illustCycle);
+    }
+}
+
+app();
